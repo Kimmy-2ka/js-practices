dab7d14bb5145c6c84be3a674475f23a1c3ab2cc
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-04 21:50:33 +0900

    luxon導入と現在年月表示を追加

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+import { DateTime } from "luxon";
 
-const monthYear = "      X月 西暦";
+const today = DateTime.now();
+
+const monthYear = `      ${today.month}月 ${today.year}`;
 const weekdays = "日 月 火 水 木 金 土";
 const days = "日付を入れる";
 
