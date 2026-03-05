918647ab3c324df030cf2a4515dca23edac98525
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-05 13:46:11 +0900

    printDaysで当月の日付を表示

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -5,6 +5,16 @@ const today = DateTime.now();
 
 const monthYear = `      ${today.month}月 ${today.year}`;
 const weekdays = "日 月 火 水 木 金 土";
-const days = "日付を入れる";
 
-console.log(`${monthYear}\n${weekdays}\n${days}`);
+console.log(`${monthYear}\n${weekdays}`);
+
+function printDays(end) {
+  for (let d = today.startOf("month"); d <= end; d = d.plus({ days: 1 })) {
+    const day = d.toFormat("d").padStart(2);
+    process.stdout.write(day);
+    if (d.weekday == 6) process.stdout.write("\n");
+    else process.stdout.write(" ");
+  }
+}
+
+printDays(today.endOf("month"));
