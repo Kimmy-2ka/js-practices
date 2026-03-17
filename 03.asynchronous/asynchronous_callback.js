9c149bce23ea17e15a775e67787a29c7565b8bf0
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-17 19:09:02 +0900

    callback形式でbooksテーブルの失敗処理を実装

--- 03.asynchronous/asynchronous_callback.js
+++ 03.asynchronous/asynchronous_callback.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 const db = new sqlite3.Database(":memory:");
 
 db.run(
@@ -19,6 +20,36 @@ db.run(
           function (_err, row) {
             console.log(row);
             db.run(`DROP TABLE books`);
+          },
+        );
+      },
+    );
+  },
+);
+
+await timers.setTimeout(100);
+
+db.run(
+  `CREATE TABLE books(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE
+  )`,
+  function () {
+    db.run(
+      `INSERT INTO books(title) VALUES(?)`,
+      ["Never Let Me Go"],
+      function () {
+        db.run(
+          `INSERT INTO books(title) VALUES(?)`,
+          ["Never Let Me Go"],
+          function (err) {
+            if (err) console.error(`${err.name}: ${err.message}`);
+
+            db.all(`SELECT * FROM book ORDER BY id`, function (err) {
+              if (err) console.error(`${err.name}: ${err.message}`);
+
+              db.run(`DROP TABLE books`);
+            });
           },
         );
       },
