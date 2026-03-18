95bbf198d342a79721db24a7c45bb1f81bb2079a
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-18 20:40:25 +0900

    promise形式でbooksテーブルの失敗処理を実装

--- 03.asynchronous/asynchronous_promise.js
+++ 03.asynchronous/asynchronous_promise.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 const db = new sqlite3.Database(":memory:");
 
 function run(sql, params) {
@@ -26,6 +27,18 @@ function get(sql, params) {
   });
 }
 
+function all(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, function (err, rows) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(rows);
+    });
+  });
+}
+
 run(
   `CREATE TABLE books(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -39,5 +52,28 @@ run(
   })
   .then((row) => {
     console.log(row);
+    return run(`DROP TABLE books`);
+  });
+
+await timers.setTimeout(100);
+
+run(
+  `CREATE TABLE books(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE
+  )`,
+)
+  .then(() => run(`INSERT INTO books(title) VALUES(?)`, ["Never Let Me Go"]))
+  .then(() => run(`INSERT INTO books(title) VALUES(?)`, ["Never Let Me Go"]))
+  .catch((err) => {
+    console.error(`${err.name}: ${err.message}`);
+  })
+  .then(() => {
+    return all(`SELECT * FROM book ORDER BY id`);
+  })
+  .catch((err) => {
+    console.error(`${err.name}: ${err.message}`);
+  })
+  .then(() => {
     return run(`DROP TABLE books`);
   });
