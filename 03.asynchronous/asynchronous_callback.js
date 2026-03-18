db950aa864541c513d35e943e6334ce9209ec97f
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-18 22:43:15 +0900

    callback/promiseのプログラムを関数化し、ケース名を出力する

--- 03.asynchronous/asynchronous_callback.js
+++ 03.asynchronous/asynchronous_callback.js
@@ -3,56 +3,64 @@ import sqlite3 from "sqlite3";
 import timers from "timers/promises";
 const db = new sqlite3.Database(":memory:");
 
-db.run(
-  `CREATE TABLE books(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL UNIQUE
-  )`,
-  function () {
-    db.run(
-      `INSERT INTO books(title) VALUES(?)`,
-      ["Never Let Me Go"],
-      function () {
-        console.log(this.lastID);
-        db.get(
-          `SELECT * FROM books WHERE id = ?`,
-          [this.lastID],
-          function (_err, row) {
-            console.log(row);
-            db.run(`DROP TABLE books`);
-          },
-        );
-      },
-    );
-  },
-);
+function successCase() {
+  db.run(
+    `CREATE TABLE books(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL UNIQUE
+    )`,
+    function () {
+      db.run(
+        `INSERT INTO books(title) VALUES(?)`,
+        ["Never Let Me Go"],
+        function () {
+          console.log(this.lastID);
+          db.get(
+            `SELECT * FROM books WHERE id = ?`,
+            [this.lastID],
+            function (_err, row) {
+              console.log(row);
+              db.run(`DROP TABLE books`);
+            },
+          );
+        },
+      );
+    },
+  );
+}
 
-await timers.setTimeout(100);
-
-db.run(
-  `CREATE TABLE books(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL UNIQUE
-  )`,
-  function () {
-    db.run(
-      `INSERT INTO books(title) VALUES(?)`,
-      ["Never Let Me Go"],
-      function () {
-        db.run(
-          `INSERT INTO books(title) VALUES(?)`,
-          ["Never Let Me Go"],
-          function (err) {
-            if (err) console.error(`${err.name}: ${err.message}`);
-
-            db.all(`SELECT * FROM book ORDER BY id`, function (err) {
+function errorCase() {
+  db.run(
+    `CREATE TABLE books(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL UNIQUE
+    )`,
+    function () {
+      db.run(
+        `INSERT INTO books(title) VALUES(?)`,
+        ["Never Let Me Go"],
+        function () {
+          db.run(
+            `INSERT INTO books(title) VALUES(?)`,
+            ["Never Let Me Go"],
+            function (err) {
               if (err) console.error(`${err.name}: ${err.message}`);
 
-              db.run(`DROP TABLE books`);
-            });
-          },
-        );
-      },
-    );
-  },
-);
+              db.all(`SELECT * FROM book ORDER BY id`, function (err) {
+                if (err) console.error(`${err.name}: ${err.message}`);
+
+                db.run(`DROP TABLE books`);
+              });
+            },
+          );
+        },
+      );
+    },
+  );
+}
+
+console.log("--エラーなしのプログラム--");
+successCase();
+await timers.setTimeout(100);
+console.log("--エラーありのプログラム--");
+errorCase();
--- 03.asynchronous/asynchronous_promise.js
+++ 03.asynchronous/asynchronous_promise.js
@@ -2,41 +2,49 @@
 import timers from "timers/promises";
 import { run, get, all } from "./modules/sqlite_promises.js";
 
-run(
-  `CREATE TABLE books(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL UNIQUE
-  )`,
-)
-  .then(() => run(`INSERT INTO books(title) VALUES(?)`, ["Never Let Me Go"]))
-  .then((statement) => {
-    console.log(statement.lastID);
-    return get(`SELECT * FROM books WHERE id = ?`, [statement.lastID]);
-  })
-  .then((row) => {
-    console.log(row);
-    return run(`DROP TABLE books`);
-  });
+function successCase() {
+  run(
+    `CREATE TABLE books(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL UNIQUE
+    )`,
+  )
+    .then(() => run(`INSERT INTO books(title) VALUES(?)`, ["Never Let Me Go"]))
+    .then((statement) => {
+      console.log(statement.lastID);
+      return get(`SELECT * FROM books WHERE id = ?`, [statement.lastID]);
+    })
+    .then((row) => {
+      console.log(row);
+      return run(`DROP TABLE books`);
+    });
+}
 
+function errorCase() {
+  run(
+    `CREATE TABLE books(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL UNIQUE
+    )`,
+  )
+    .then(() => run(`INSERT INTO books(title) VALUES(?)`, ["Never Let Me Go"]))
+    .then(() => run(`INSERT INTO books(title) VALUES(?)`, ["Never Let Me Go"]))
+    .catch((err) => {
+      console.error(`${err.name}: ${err.message}`);
+    })
+    .then(() => {
+      return all(`SELECT * FROM book ORDER BY id`);
+    })
+    .catch((err) => {
+      console.error(`${err.name}: ${err.message}`);
+    })
+    .then(() => {
+      return run(`DROP TABLE books`);
+    });
+}
+
+console.log("--エラーなしのプログラム--");
+successCase();
 await timers.setTimeout(100);
-
-run(
-  `CREATE TABLE books(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL UNIQUE
-  )`,
-)
-  .then(() => run(`INSERT INTO books(title) VALUES(?)`, ["Never Let Me Go"]))
-  .then(() => run(`INSERT INTO books(title) VALUES(?)`, ["Never Let Me Go"]))
-  .catch((err) => {
-    console.error(`${err.name}: ${err.message}`);
-  })
-  .then(() => {
-    return all(`SELECT * FROM book ORDER BY id`);
-  })
-  .catch((err) => {
-    console.error(`${err.name}: ${err.message}`);
-  })
-  .then(() => {
-    return run(`DROP TABLE books`);
-  });
+console.log("--エラーありのプログラム--");
+errorCase();
