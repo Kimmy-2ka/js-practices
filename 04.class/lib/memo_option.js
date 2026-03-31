3838bf717d288d5ff262845cb3fe97018eee390f
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-31 22:48:38 +0900

    MemoOptionクラスを作成

---
+++ 04.class/lib/memo_option.js
@@ -0,0 +1,44 @@
+import { Command, Option } from "commander";
+
+export class MemoOption {
+  constructor() {
+    this.options = this.#createOptions();
+  }
+
+  hasList() {
+    return this.options.list;
+  }
+
+  hasReference() {
+    return this.options.reference;
+  }
+
+  hasDelete() {
+    return this.options.delete;
+  }
+
+  #createOptions() {
+    const program = new Command();
+    program
+      .addOption(
+        new Option("-l, --list", "list memos").conflicts([
+          "reference",
+          "delete",
+        ]),
+      )
+      .addOption(
+        new Option("-r, --reference", "reference a memo").conflicts([
+          "list",
+          "delete",
+        ]),
+      )
+      .addOption(
+        new Option("-d, --delete", "delete a memo").conflicts([
+          "list",
+          "reference",
+        ]),
+      );
+    program.parse();
+    return program.opts();
+  }
+}
